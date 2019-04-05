74e448f2c2fcf26f8b651b437b23e238714a47cb
Jakub Kovac <kovadlinak@gmail.com>
2019-04-05 09:20:05 +0200

    modified vedas str_to_title

--- mcnue.R
+++ mcnue.R
@@ -5,6 +5,17 @@ library(pander)
 
 #run web scraper
 R.utils::sourceDirectory("Restaurants")
+
+str_to_1up <- function(x) {
+  substr(x, 1, 1) <- toupper(substr(x, 1, 1))
+  x
+}
+
+str_remove_1gl <- function(x) {
+  substr(x,1,1)[substr(x,1,1) == "l" | substr(x,1,1) == "g"] <- ""
+  x
+}
+
 menu <- tibble(podnik = character(), polievka = character(), jedlo_1 = character(), jedlo_2 = character(), jedlo_3 = character(), jedlo_4 = character())
 (menu[1,] <- bioland())
 (menu[2,] <- bluebear())
@@ -18,29 +29,36 @@ menu <- tibble(podnik = character(), polievka = character(), jedlo_1 = character
 (menu[10,] <- realstar())
 (menu2 <- lenka())
 
+
+
 #menu <- na.omit(menu)
 #remove special slovak characters
 source("slovak_language_destroyer.R",encoding="utf-8")
 menu <-
   menu %>%
-  mutate_all(.fun = function(x) slovak_language_destroyer(x))
+  mutate_all(.fun = slovak_language_destroyer)
 
 menu2 <-
   menu2 %>%
-  mutate_all(.fun = function(x) slovak_language_destroyer(x))
+  mutate_all(.fun = slovak_language_destroyer)
 
 #destroy the nuances in data
 source("benson_string_destroyer.R")
 menu <-
   menu %>%
-  mutate_all(.fun = function(x) benson_string_destroyer(x))
+  mutate_all(.fun = benson_string_destroyer)
 menu
 menu[10,] <- menu[10,] %>% str_replace("  l","") %>% str_replace("  g","") %>% str_trim()
 
+menu <-
+  menu %>%
+  mutate_all(.fun = str_remove_1gl)
 #randomize order
 #menu <- menu[sample(1:nrow(menu),nrow(menu)),]
-
-
+menu <-
+  menu %>%
+  mutate_all(.fun =  str_to_1up)
+menu <- menu %>% arrange(podnik)
 menu2
 #save the menu in a text file as an ascii table
 menu_ascii <- pandoc.table.return(menu, style = "grid", split.tables = Inf, split.cells = 30)
